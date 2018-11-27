f9b6390fccff5b3174cacf207fc1ddfadf94b843
Bruno Pimentel <brunoapimentel@gmail.com>
2018-11-27 21:20:23 -0200

    Cria componente Timer

--- src/components/caixa/Caixa.js
+++ src/components/caixa/Caixa.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import './Caixa.css';
+import Timer from '../timer/Timer';
 
 class Caixa extends React.Component{
     render(){
         return <section className="Caixa">
             <div className="main">
-                <div className="timer">
-                <span>00</span>
-                <span>00</span>
+                <Timer/>
+
+                <div className="buttons">
+                    <button>+</button>
+                    <button>-</button>
+                </div>
             </div>
 
             <div className="buttons">
-                <button>+</button>
-                <button>-</button>
+                <button>Iniciar</button>
+                <button>Zerar</button>
             </div>
-        </div>
-
-        <div className="buttons">
-            <button>Iniciar</button>
-            <button>Zerar</button>
-        </div>
       </section>
     }
 }
---
+++ src/components/timer/Timer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Timer.css';
+
+class Timer extends React.Component{
+    render(){
+        return <div className="Timer">
+            <span>00</span>
+            <span>00</span>
+        </div>
+    }
+}
+
+export default Timer;
