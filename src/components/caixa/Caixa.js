c3f8a9410f14edd9effd9d022b724967f78cb1ea
murillocesar <murillocbispo@gmail.com>
2018-11-28 21:35:10 -0200

    modificaçao do caixa.js

--- src/components/caixa/Caixa.js
+++ src/components/caixa/Caixa.js
@@ -4,7 +4,37 @@ import Timer from '../timer/Timer';
 import Botao from '../botao/Botao';
 
 class Caixa extends React.Component{
+
+    constructor(){
+        super();
+
+        this.minutos = 4;
+        this.segundos =20;
+    }
+
+    decrementar(){
+        setInterval(() =>{
+            let meusSegundos = this.state.segundos;
+            let meusMinutos = this.state.minutos;
+            
+            if(meusMinutos=== 0 && meusSegundos === 0){
+                clearInterval(intervalo);
+            }
+            else if(meusSegundos == 0){
+                meusSegundos = 59;
+                meusMinutos--;
+            }
+            else{
+                meusSegundos--;
+            }
+            this.setState({segundos: meusSegundos});
+        }, 1000);
+    }
+
+
     render(){
+        let minutos = 4;
+        let segundos = 20;
         return <section className="Caixa">
             <div className="main">
                 <Timer minutos="10" segundos="23"/>
